04502b495392683be4b7fc27473f9e1045861010
Sanjeev11Krishna <sanjeev11stokes@gmail.com>
2025-03-02 16:05:29 +0530

    Add files via upload

---
+++ Lecture 1/Introduction to CS and Python.py
@@ -0,0 +1,102 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> type(7)
+<class 'int'>
+>>> type(3.0)
+<class 'float'>
+>>> type(True)
+<class 'bool'>
+>>> type(False)
+<class 'bool'>
+>>> type(1234)
+<class 'int'>
+>>> type(8.99)
+<class 'float'>
+>>> type(9.0)
+<class 'float'>
+>>> float(3)
+3.0
+>>> int(5.2)
+5
+>>> int(5.9)
+5
+>>> round(5.9)
+6
+>>> float(123)
+123.0
+>>> round(7.9)
+8
+>>> float(round(7,2))
+7.0
+>>> int(7,2)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    int(7,2)
+TypeError: int() can't convert non-string with explicit base
+>>> int(7.2)
+7
+>>> 3 + 2
+5
+>>> (4 + 2) * 6 - 1
+35
+type((4 + 2) * 6 - 1)
+<class 'int'>
+float((4 + 2) * 6 - 1)
+35.0
+(13 - 4) / (12 * 12)
+0.0625
+type(4 * 3)
+<class 'int'>
+type(4.0 * 3)
+<class 'float'>
+int(1 / 2)
+0
+5 / 3
+1.6666666666666667
+5 // 3
+1
+5 % 3
+2
+5 % 3
+2
+x = 6
+xy = 3 + 4
+xy
+7
+xy + 1
+8
+pi = 355 / 133
+radius = 2.2
+area = pi * (radius * 2)
+circumference = pi * (radius * 2)
+a = 355 / 133 * (2.2 * 2)
+c = 355 / 133 * (2.2 * 2)
+a = c
+meters = 100
+feet = 3.2802 * meters
+meters = 2mm
+SyntaxError: invalid decimal literal
+meters = 200
+feet
+328.02
+meters
+200
+x = 1
+y = 2
+y = x
+y
+1
+x = y
+x
+1
+y = 2
+y = x
+x = 1
+y = 2
+temp = y
+y = x
+x = temp
+x
+2
+y
+1
