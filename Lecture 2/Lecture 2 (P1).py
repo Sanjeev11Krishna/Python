f4d0121bd965ddcf5e437335f519e9eb585ade1a
Sanjeev11Krishna <sanjeev11stokes@gmail.com>
2025-03-03 19:31:09 +0530

    Add files via upload

---
+++ Lecture 2/Lecture 2 (P1).py
@@ -0,0 +1,20 @@
+a = "the"
+b = 3
+c = "musketeer"
+print(a, b, c) # output : the 3 musketeer
+print(a + str(b) + c) # output: the3musketeer
+
+#Input commad
+text = input("Type antything : ") #input : Hello
+print(5 * text) #output: HelloHelloHelloHelloHello
+
+num1 = input("Type a number : ") #input: 1
+print(5 * num1) #output: 11111
+
+num2 = int(input("Type a number : ")) # input: 1
+print(5 * num2) # output: 5
+
+#reading a input from the user and inserting it in a sentence
+que = input('Choose a verb : ') #input: sing
+print('I can ', que, 'better than you') #output: I can sing better than you
+print(que * 5) #output: singsingsingsingsing
